0258a54970fcfb2f2df92659298288646e1b08de
H P <13865358206hp@gmail.com>
2026-05-14 17:20:56 +0800

    简化MATLAB启动：bat文件移至根目录，删除旧文件

---
+++ matlab.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d %USERPROFILE%\Desktop\lihongwei-ch\matlab
+start matlab.exe
+echo MATLAB R2016b 已启动
+echo 工作目录自动设为 matlab/
+echo.
+echo 首次使用请在 Command Window 输入: startup_setup
+echo 然后可直接运行: vehicle_dynamics
--- tools/matlab_start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d %USERPROFILE%\Desktop\lihongwei-ch\matlab
-echo MATLAB 工作目录已设为: %CD%
-echo.
-echo 请在 MATLAB 的 Command Window 中运行:
-echo   startup_setup
-echo.
-start matlab.exe
