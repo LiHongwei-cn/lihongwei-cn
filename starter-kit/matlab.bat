38a016b01b4c040c63c0b6c0e2098bbebe77a00b
H P <13865358206hp@gmail.com>
2026-05-14 18:10:31 +0800

    autosave 2026-05-14 18:10:31

---
+++ starter-kit/matlab.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d %USERPROFILE%\Desktop\YOUR_REPO_NAME\matlab
+start matlab.exe
+echo MATLAB 已启动
+echo 工作目录自动设为 matlab/
+echo.
+echo 首次使用请在 Command Window 输入: startup_setup
+echo 然后可直接运行: vehicle_dynamics
+pause
