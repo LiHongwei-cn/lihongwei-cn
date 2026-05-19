0a2f47c9ee9e6b6a76f60c7f50ff725bf1db8155
H P <13865358206hp@gmail.com>
2026-05-19 19:03:22 +0800

    autosave 2026-05-19 19:03:22

---
+++ bp-monitor/upload.cmd
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" upload --project "C:\Users\HP\Desktop\1\bp-monitor\miniprogram" --version "1.0.2" --desc "optimized: loading, pull-refresh, med-edit, quick-nav"
+exit /b %ERRORLEVEL%
