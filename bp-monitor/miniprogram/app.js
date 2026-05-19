f016e51e30d93a51e046dd5188aee4802ddd3450
H P <13865358206hp@gmail.com>
2026-05-19 18:42:50 +0800

    autosave 2026-05-19 18:42:50

--- bp-monitor/miniprogram/app.js
+++ bp-monitor/miniprogram/app.js
@@ -2,7 +2,7 @@ App({
   globalData: {
     token: '',
     userInfo: null,
-    apiBase: 'https://c68c8477a4df35.lhr.life/api'
+    apiBase: 'https://e60660bb9b6018.lhr.life/api'
   },
 
   onLaunch() {
