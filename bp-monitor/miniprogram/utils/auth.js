4acb005fde48fb8ce95c1cd131e033418f857581
H P <13865358206hp@gmail.com>
2026-05-19 23:27:17 +0800

    autosave 2026-05-19 23:27:17

--- bp-monitor/miniprogram/utils/auth.js
+++ bp-monitor/miniprogram/utils/auth.js
@@ -1,17 +1,20 @@
 const app = getApp();
 const api = require('./api.js');
 
-const DEV_FALLBACK = 'dev_preview_fallback';
-
 function login() {
   return new Promise((resolve, reject) => {
     wx.login({
       success: (res) => {
-        const code = (res.code && res.code.length > 0) ? res.code : DEV_FALLBACK;
-        doAuth(code, resolve, reject);
+        const code = (res.code && res.code.length > 0) ? res.code : null;
+        if (code) {
+          doAuth(code, resolve, reject);
+        } else {
+          // wx.login 返回了空 code（开发工具未登录微信账号时出现）
+          reject(new Error('微信登录未就绪，请在开发者工具中登录微信账号，或确认后端 DEV_MODE=true'));
+        }
       },
-      fail: () => {
-        doAuth(DEV_FALLBACK, resolve, reject);
+      fail: (err) => {
+        reject(new Error('wx.login 调用失败: ' + (err.errMsg || '未知错误')));
       }
     });
   });
@@ -25,7 +28,9 @@ function doAuth(code, resolve, reject) {
       wx.setStorageSync('token', data.token);
       resolve(data.user);
     })
-    .catch(reject);
+    .catch((err) => {
+      reject(new Error('登录请求失败，请确认后端服务已启动（' + err.message + '）'));
+    });
 }
 
 module.exports = { login };
